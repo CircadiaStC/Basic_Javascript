213e52519e7bc03fe212328e487d9abfb7401821
CircadiaStC <83201571+CircadiaStC@users.noreply.github.com>
2021-06-18 22:07:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr.Seuss");
+var A="This is a string";
+document.write(A);
+var B="Concatenated" + "String"
+document.write(B)
+var Family="The Arezzinis", Dad="Jeremiah", Mom="Hermione",
+    Daughter="Penny", Son="Zorro";
+   
+document.write(Dad);
+
+
+var blues="I have the blues.";      //defines a variable
+var blues=blues.fontcolor("blue"); //makes the font blue
+document.write(blues);             //writes the string variable in the text
+A=A.fontcolor("red");              //makes the font red
+document.write(A);                  //writes the string variable in the text
+B=B.fontcolor("orange");
+document.write(B);
+Family=Family.fontcolor("magenta");
+document.write(Family);
+Dad=Dad.fontcolor("Green");
+document.write(Dad);
+Mom=Mom.fontcolor("purple");
+document.write(Mom);
+Daughter=Daughter.fontcolor("#00FFFF");
+document.write(Daughter);
+Son=Son.fontcolor("#CD5C5C");
+document.write(Son);
+document.write(3+3);            //an expression within the text
